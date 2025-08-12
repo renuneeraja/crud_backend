abd15d62e40d66fd49d871653929560c9249a48d
renuneeraja <142807692+renuneeraja@users.noreply.github.com>
2025-08-12 12:03:55 +0530

    Update AppController.java

--- crud_backend-main/src/main/java/com/klu/AppController.java
+++ crud_backend-main/src/main/java/com/klu/AppController.java
@@ -33,10 +33,9 @@ public class AppController {
 		return s.updateData(p);
 	}
 
-	/* 
+
 	@DeleteMapping("/delete/{id}")
 	public String delete(@PathVariable int id) {
 		return s.deleteData(id);
 	}
-	*/
 }
